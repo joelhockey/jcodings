82df4b607fc34c358647c834c849309d39661a44
Joel Hockey <joel.hockey@gmail.com>
2011-01-11 23:19:00 +1000

    Added new casefold config flag ENC_CASE_FOLD_NO_UNICODE_TO_ASCII which forces javascript-style case matching where unicode chars will not fold to ascii

--- src/org/jcodings/unicode/UnicodeEncoding.java
+++ src/org/jcodings/unicode/UnicodeEncoding.java
@@ -1,20 +1,20 @@
 /*
- * Permission is hereby granted, free of charge, to any person obtaining a copy of 
- * this software and associated documentation files (the "Software"), to deal in 
- * the Software without restriction, including without limitation the rights to 
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
  * of the Software, and to permit persons to whom the Software is furnished to do
  * so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
 package org.jcodings.unicode;
@@ -39,7 +39,7 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
     }
 
     protected UnicodeEncoding(String name, int minLength, int maxLength, int[]EncLen, int[][]Trans) {
-        // ASCII type tables for all Unicode encodings        
+        // ASCII type tables for all Unicode encodings
         super(name, minLength, maxLength, EncLen, Trans, UNICODE_ISO_8859_1_CTypeTable);
     }
 
@@ -48,7 +48,7 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
     public boolean isCodeCType(int code, int ctype) {
         if (Config.USE_UNICODE_PROPERTIES) {
             if (ctype <= CharacterType.MAX_STD_CTYPE && code < 256)
-                return isCodeCTypeInternal(code, ctype); 
+                return isCodeCTypeInternal(code, ctype);
         } else {
             if (code < 256) return isCodeCTypeInternal(code, ctype);
         }
@@ -56,24 +56,24 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
         if (ctype > UnicodeCodeRanges.CodeRangeTable.length) throw new InternalError(ErrorMessages.ERR_TYPE_BUG);
 
         return CodeRange.isInCodeRange(UnicodeCodeRanges.CodeRangeTable[ctype], code);
-        
+
     }
-    
+
     // onigenc_unicode_ctype_code_range
     protected final int[]ctypeCodeRange(int ctype) {
         if (ctype >= UnicodeCodeRanges.CodeRangeTable.length) throw new InternalError(ErrorMessages.ERR_TYPE_BUG);
-        
+
         return UnicodeCodeRanges.CodeRangeTable[ctype];
     }
-    
+
     // onigenc_unicode_property_name_to_ctype
     @Override
     public int propertyNameToCType(byte[]name, int p, int end) {
         byte[]buf = new byte[PROPERTY_NAME_MAX_SIZE];
-        
+
         int p_ = p;
         int len = 0;
-        
+
         while(p_ < end) {
             int code = mbcToCode(name, p_, end);
             if (code >= 0x80) throw new CharacterPropertyException(ErrorMessages.ERR_INVALID_CHAR_PROPERTY_NAME);
@@ -84,19 +84,19 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
 
         Integer ctype = UnicodeCTypeNames.CTypeNameHash.get(buf, 0, len);
         if (ctype == null) throw new CharacterPropertyException(ErrorMessages.ERR_INVALID_CHAR_PROPERTY_NAME, name, p, end);
-        return ctype;       
+        return ctype;
     }
-    
+
     // onigenc_unicode_mbc_case_fold
     @Override
     public int mbcCaseFold(int flag, byte[]bytes, IntHolder pp, int end, byte[]fold) {
         int p = pp.value;
         int foldP = 0;
-        
+
         int code = mbcToCode(bytes, p, end);
         int len = length(bytes, p, end);
         pp.value += len;
-        
+
         if (Config.USE_UNICODE_CASE_FOLD_TURKISH_AZERI) {
             if ((flag & Config.ENC_CASE_FOLD_TURKISH_AZERI) != 0) {
                 if (code == 0x0049) {
@@ -106,7 +106,7 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
                 }
             }
         }
-        
+
         int to[] = UnicodeCaseFolds.FoldHash.get(code);
         if (to != null) {
             if (to.length == 1) {
@@ -114,7 +114,7 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
             } else {
                 int rlen = 0;
                 for (int i=0; i<to.length; i++) {
-                    len = codeToMbc(to[i], fold, foldP); 
+                    len = codeToMbc(to[i], fold, foldP);
                     foldP += len;
                     rlen += len;
                 }
@@ -127,35 +127,44 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
         }
         return len;
     }
-    
+
     // onigenc_unicode_apply_all_case_fold
     @Override
     public void applyAllCaseFold(int flag, ApplyAllCaseFoldFunction fun, Object arg) {
-        /* if (CaseFoldInited == 0) init_case_fold_table(); */      
+        /* if (CaseFoldInited == 0) init_case_fold_table(); */
 
+        boolean noUnicodeToAscii =
+            (flag & Config.ENC_CASE_FOLD_NO_UNICODE_TO_ASCII) != 0;
+        int[]toAscii = new int[]{0};
         int[]code = new int[]{0};
         for (int i=0; i<UnicodeCaseFolds.CaseUnfold_11_From.length; i++) {
             int from = UnicodeCaseFolds.CaseUnfold_11_From[i];
-            int[]to = UnicodeCaseFolds.CaseUnfold_11_To[i];
-            
+            int[]to = null;
+            if (noUnicodeToAscii && isAscii(from)) {
+                toAscii[0] = asciiToLower(from);
+                to = toAscii;
+            } else {
+                to = UnicodeCaseFolds.CaseUnfold_11_To[i];
+            }
+
             for (int j=0; j<to.length; j++) {
                 code[0] = from;
                 fun.apply(to[j], code, 1, arg);
-                
+
                 code[0] = to[j];
                 fun.apply(from, code, 1, arg);
-                
+
                 for (int k=0; k<j; k++) {
                     code[0] = to[k];
                     fun.apply(to[j], code, 1, arg);
-                    
+
                     code[0] = to[j];
                     fun.apply(to[k], code, 1, arg);
                 }
-                
+
             }
         }
-        
+
         if (Config.USE_UNICODE_CASE_FOLD_TURKISH_AZERI && (flag & Config.ENC_CASE_FOLD_TURKISH_AZERI) != 0) {
             code[0] = 0x0131;
             fun.apply(0x0049, code, 1, arg);
@@ -168,33 +177,39 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
         } else {
             for (int i=0; i<UnicodeCaseFolds.CaseUnfold_11_Locale_From.length; i++) {
                 int from = UnicodeCaseFolds.CaseUnfold_11_Locale_From[i];
-                int[]to = UnicodeCaseFolds.CaseUnfold_11_Locale_To[i];
-            
+                int[]to = null;
+                if (noUnicodeToAscii && isAscii(from)) {
+                    toAscii[0] = asciiToLower(from);
+                    to = toAscii;
+                } else {
+                    to = UnicodeCaseFolds.CaseUnfold_11_Locale_To[i];
+                }
+
                 for (int j=0; j<to.length; j++) {
                     code[0] = from;
                     fun.apply(to[j], code, 1, arg);
-                
+
                     code[0] = to[j];
                     fun.apply(from, code, 1, arg);
-                
+
                     for (int k = 0; k<j; k++) {
                         code[0] = to[k];
                         fun.apply(to[j], code, 1, arg);
-                    
+
                         code[0] = to[j];
                         fun.apply(to[k], code, 1, arg);
                     }
                 }
             }
         } // USE_UNICODE_CASE_FOLD_TURKISH_AZERI
-        
+
         if ((flag & Config.INTERNAL_ENC_CASE_FOLD_MULTI_CHAR) != 0) {
             for (int i=0; i<UnicodeCaseFolds.CaseUnfold_12.length; i+=2) {
                 int[]from = UnicodeCaseFolds.CaseUnfold_12[i];
                 int[]to = UnicodeCaseFolds.CaseUnfold_12[i + 1];
                 for (int j=0; j<to.length; j++) {
                     fun.apply(to[j], from, 2, arg);
-                    
+
                     for (int k=0; k<to.length; k++) {
                         if (k == j) continue;
                         code[0] = to[k];
@@ -218,14 +233,14 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
                     }
                 }
             } // !USE_UNICODE_CASE_FOLD_TURKISH_AZERI
-            
+
             for (int i=0; i<UnicodeCaseFolds.CaseUnfold_13.length; i+=2) {
                 int[]from = UnicodeCaseFolds.CaseUnfold_13[i];
                 int[]to = UnicodeCaseFolds.CaseUnfold_13[i + 1];
-                
+
                 for (int j=0; j<to.length; j++) {
                     fun.apply(to[j], from, 3, arg); //// ????
-                    
+
                     for (int k=0; k<to.length; k++) {
                         if (k == j) continue;
                         code[0] = to[k];
@@ -233,10 +248,10 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
                     }
                 }
             }
-            
+
         } // INTERNAL_ENC_CASE_FOLD_MULTI_CHAR
     }
-    
+
     // onigenc_unicode_get_case_fold_codes_by_str
     @Override
     public CaseFoldCodeItem[]caseFoldCodesByString(int flag, byte[]bytes, int p, int end) {
@@ -248,7 +263,7 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
                 if (code == 0x0049) {
                     return new CaseFoldCodeItem[]{new CaseFoldCodeItem(len, 1, new int[]{0x0131})};
                 } else if(code == 0x0130) {
-                    return new CaseFoldCodeItem[]{new CaseFoldCodeItem(len, 1, new int[]{0x0069})};                    
+                    return new CaseFoldCodeItem[]{new CaseFoldCodeItem(len, 1, new int[]{0x0069})};
                 } else if(code == 0x0131) {
                     return new CaseFoldCodeItem[]{new CaseFoldCodeItem(len, 1, new int[]{0x0049})};
                 } else if(code == 0x0069) {
@@ -256,21 +271,21 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
                 }
             }
         } // USE_UNICODE_CASE_FOLD_TURKISH_AZERI
-        
+
         int n = 0;
         int fn = 0;
-        
+
         int[]to = UnicodeCaseFolds.FoldHash.get(code);
         CaseFoldCodeItem[]items = null;
         if (to != null) {
             items = new CaseFoldCodeItem[Config.ENC_GET_CASE_FOLD_CODES_MAX_NUM];
-            
+
             if (to.length == 1) {
                 int origCode = code;
-                
+
                 items[0] = new CaseFoldCodeItem(len, 1, new int[]{to[0]});
                 n++;
-                
+
                 code = to[0];
                 to = UnicodeCaseFolds.Unfold1Hash.get(code);
                 if (to != null) {
@@ -284,7 +299,7 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
             } else if ((flag & Config.INTERNAL_ENC_CASE_FOLD_MULTI_CHAR) != 0) {
                 int[][]cs = new int[3][4];
                 int[]ncs = new int[3];
-                
+
                 for (fn=0; fn<to.length; fn++) {
                     cs[fn][0] = to[fn];
                     int[]z3 = UnicodeCaseFolds.Unfold1Hash.get(cs[fn][0]);
@@ -305,13 +320,13 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
                             n++;
                         }
                     }
-                        
+
                     int[]z2 = UnicodeCaseFolds.Unfold2Hash.get(to);
                     if (z2 != null) {
                         for (int i=0; i<z2.length; i++) {
                             if (z2[i] == code) continue;
                             items[n] = new CaseFoldCodeItem(len, 1, new int[]{z2[i]});
-                            n++;                                
+                            n++;
                         }
                     }
                 } else {
@@ -327,7 +342,7 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
                     if (z2 != null) {
                         for (int i=0; i<z2.length; i++) {
                             if (z2[i] == code) continue;
-                            items[n] = new CaseFoldCodeItem(len, 1, new int[]{z2[i]});    
+                            items[n] = new CaseFoldCodeItem(len, 1, new int[]{z2[i]});
                             n++;
                         }
                     }
@@ -345,22 +360,22 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
                 }
             }
         }
-        
+
         if ((flag & Config.INTERNAL_ENC_CASE_FOLD_MULTI_CHAR) != 0) {
             if (items == null) items = new CaseFoldCodeItem[Config.ENC_GET_CASE_FOLD_CODES_MAX_NUM];
-            
+
             p += len;
-            if (p < end) {                
+            if (p < end) {
                 int[]codes = new int[3];
                 codes[0] = code;
                 code = mbcToCode(bytes, p, end);
                 to = UnicodeCaseFolds.FoldHash.get(code);
                 if (to != null && to.length == 1) {
-                    codes[1] = to[0]; 
+                    codes[1] = to[0];
                 } else {
                     codes[1] = code;
                 }
-                    
+
                 int clen = length(bytes, p, end);
                 len += clen;
                 int[]z2 = UnicodeCaseFolds.Unfold2Hash.get(codes);
@@ -391,7 +406,7 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
                 }
             }
         }
-        
+
         if (items == null || n == 0) return EMPTY_FOLD_CODES;
         if (n < items.length) {
             CaseFoldCodeItem [] tmp = new CaseFoldCodeItem[n];
@@ -401,7 +416,7 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
             return items;
         }
     }
-    
+
     static final short UNICODE_ISO_8859_1_CTypeTable[] = {
           0x4008, 0x4008, 0x4008, 0x4008, 0x4008, 0x4008, 0x4008, 0x4008,
           0x4008, 0x428c, 0x4289, 0x4288, 0x4288, 0x4288, 0x4008, 0x4008,
@@ -435,5 +450,5 @@ public abstract class UnicodeEncoding extends MultiByteEncoding {
           0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2,
           0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x00a0,
           0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2, 0x30e2
-    };      
+    };
 }
